36061bd93d9161fb76b953f96f3ca3d6a194efd1
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-15 18:23:35 -0300

    feat: get anime episode URL route

--- src/controller/animes.ts
+++ src/controller/animes.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 import { listAnimes } from "../service/animes/listAnimes";
 import { searchAnimes } from "../service/animes/searchAnimes";
 import { getAnimeInfos } from "../service/animes/getAnimeInfos";
+import { getAnimeEpisodeUrl } from "../service/animes/getAnimeEpisodeUrl";
 
 export const animesController = new Elysia().group("/animes", (app) =>
   app
@@ -9,6 +10,11 @@ export const animesController = new Elysia().group("/animes", (app) =>
       const animes = await searchAnimes(params.param);
       if (!animes) set.status = 404;
       return animes;
+    })
+    .get("/watch/:anime/:episode", async ({ params, set }) => {
+      const url = await getAnimeEpisodeUrl(params.anime, params.episode);
+      if (!url) set.status = 404;
+      return url;
     })
     .get("/:category?/:page?", async ({ params, query, set }) => {
       const animes = await listAnimes(
