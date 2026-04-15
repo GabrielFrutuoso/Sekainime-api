49b2aeb9a05a484cc1435a65d227a14bdf294502
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-15 14:06:56 -0300

    feat: search animes route

--- src/controller/animes.ts
+++ src/controller/animes.ts
@@ -1,14 +1,21 @@
 import { Elysia } from "elysia";
 import { listAnimes } from "../service/animes/listAnimes";
+import { searchAnimes } from "../service/animes/searchAnimes";
 
 export const animesController = new Elysia().group("/animes", (app) =>
-  app.get("/:category?/:page?", async ({ params, query, set }) => {
-    const animes = await listAnimes(
-      params.category,
-      params.page ? Number(params.page) : null,
-      query.ano,
-    );
-    if (!animes) set.status = 404;
-    return animes;
-  }),
+  app
+    .get("/search/:param", async ({ params, set }) => {
+      const animes = await searchAnimes(params.param);
+      if (!animes) set.status = 404;
+      return animes;
+    })
+    .get("/:category?/:page?", async ({ params, query, set }) => {
+      const animes = await listAnimes(
+        params.category,
+        params.page ? Number(params.page) : null,
+        query.ano,
+      );
+      if (!animes) set.status = 404;
+      return animes;
+    }),
 );
